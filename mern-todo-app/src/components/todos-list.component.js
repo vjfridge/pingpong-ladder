09dadfa79201b8d1e6ddf6f91daab53b021325a9
vjfridge <vjfridge@gmail.com>
2019-07-16 16:21:10 -0400

    edit functionality implemented with backend; finished MERN Stack tutorial

--- mern-todo-app/src/components/todos-list.component.js
+++ mern-todo-app/src/components/todos-list.component.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 
 const Todo = props => (
     <tr>
-        <td>{props.todo.todo_description}</td>
-        <td>{props.todo.todo_responsible}</td>
-        <td>{props.todo.todo_priority}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
         <td>
             <Link to={'/edit/'+props.todo._id}>Edit</Link>
         </td>
@@ -21,6 +21,16 @@ export default class TodosList extends Component {
     }
 
     componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({todos: response.data});
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
+    componentDidUpdate() {
         axios.get('http://localhost:4000/todos/')
             .then(response => {
                 this.setState({todos: response.data});
